b1225f8db89af45da3c0561704f6a2905ffc56af
Luiz Evangelista <luizhenrique.se@gmail.com>
2020-12-29 21:47:25 -0300

    entendendo state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div>
-      massa
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    nome: ''
+  }
+
+  render(){
+    return(
+      <div>
+        <label>Nome:</label>
+        <input type="text" value={this.state.nome} onChange={(e) => this.setState( {nome: e.target.value} )} />
+        
+
+        O nome digitado foi: {this.state.nome}
+      
+      </div>
+    )
+  }
 }
 
 export default App;
