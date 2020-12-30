13cdda2a6abc7046428efccbfe84facc7703be9f
Luiz Evangelista <luizhenrique.se@gmail.com>
2020-12-29 22:07:12 -0300

    adicionando bootswatch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
+import 'bootswatch/dist/flatly/bootstrap.css'
 
 class App extends React.Component {
-
-  state = {
-    nome: ''
-  }
-
   render(){
     return(
       <div>
-        <label>Nome:</label>
-        <input type="text" value={this.state.nome} onChange={(e) => this.setState( {nome: e.target.value} )} />
-        
-
-        O nome digitado foi: {this.state.nome}
-      
+        <button type="button" className="btn btn-primary">Primary</button>
       </div>
     )
   }
