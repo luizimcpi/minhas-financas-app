a395a1045abb1f33c2c89956ad74fcb4a9cd2d33
Luiz Evangelista <luizhenrique.se@gmail.com>
2021-01-02 13:02:30 -0300

    implementando cadastro usuario view

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import 'bootswatch/dist/flatly/bootstrap.css'
 import Login from './views/login'
+import CadastroUsuario from './views/cadastroUsuario'
 import './custom.css'
 
 class App extends React.Component {
   render(){
     return(
       <div>
-        <Login />
+        <CadastroUsuario />
       </div>
     )
   }
