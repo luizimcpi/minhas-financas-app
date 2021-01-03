b6af8c5c967129987b40178e17f51e3c61b1f740
Luiz Evangelista <luizhenrique.se@gmail.com>
2021-01-03 12:15:59 -0300

    adicionando home

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -1,6 +1,8 @@
 import React from 'react'
+
 import Login from '../views/login'
 import CadastroUsuario from '../views/cadastroUsuario'
+import Home from '../views/home'
 
 import { Route, Switch, HashRouter } from 'react-router-dom'
 
@@ -8,6 +10,7 @@ function Rotas(){
     return (
         <HashRouter>
             <Switch>
+                <Route path="/home" component={Home} />
                 <Route path="/login" component={Login} />
                 <Route path="/cadastro-usuarios" component={CadastroUsuario} />
             </Switch>
