c6dae815ccd659351472d16b0c09aa2a4ef00af4
Luiz Evangelista <luiz.evangelista@meupag.com.br>
2021-01-05 10:17:24 -0300

    importando primereact

--- src/main/App.js
+++ src/main/App.js
@@ -2,9 +2,12 @@ import 'bootswatch/dist/flatly/bootstrap.css'
 import '../custom.css'
 import 'toastr/build/toastr.css'
 import 'toastr/build/toastr.min.js'
+import 'primereact/resources/themes/saga-blue/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
 
 import Navbar from '../components/navbar'
-import React from 'react';
+import React from 'react'
 import Rotas from './rotas'
 
 class App extends React.Component {
