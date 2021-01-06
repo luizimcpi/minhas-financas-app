606d551637a801cb734fb2cf0a0552977583784d
Luiz Evangelista <luizhenrique.se@gmail.com>
2021-01-05 21:16:59 -0300

    alterando icones dos botoes da home e adicionando url local no apiservice

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const httpClient = axios.create({
     baseURL: 'https://financas-lh-api.herokuapp.com'
+    // baseURL: 'http://localhost:8080'
 })
 
 class ApiService {
